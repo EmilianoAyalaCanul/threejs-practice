836e51aeea29ee72c0744ac180d8398096f8951f
EmilianoAyalaCanul <emiliano_ayalacanul@hotmail.com>
2026-01-09 01:24:31 -0600

    Set up basic Three.js scene

--- 10-Textures/script.js
+++ 10-Textures/script.js
@@ -1,1 +1,93 @@
-console.log("conexion con exito")
+import * as THREE from 'three'
+import GUI from 'lil-gui'
+import { OrbitControls } from 'three/examples/jsm/Addons.js'  
+
+//canvas
+const canvas = document.querySelector('canvas.webgl')
+//bound sizes
+const sizes ={
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+//textures
+const textureLoader = new THREE.TextureLoader()
+
+//object axes helper
+const axesHelper = new THREE.AxesHelper(2)
+
+//object 01 box
+const box_geometry = new THREE.BoxGeometry(1,1,1)
+const box_material = new THREE.MeshBasicMaterial({color: 0x00ff00})
+const Box_object = new THREE.Mesh(box_geometry,box_material)
+
+//Debug Grafic User Interface (GUI)
+const gui = new GUI({
+    title: 'Debug User Interface',
+    width: 300,
+})
+
+//box gui
+const box_gui = gui.addFolder('Box Item')
+box_gui
+    .add(Box_object,'visible')
+
+
+//camera
+const camera = new THREE.PerspectiveCamera(75,sizes.width/sizes.height,0.1,200)
+camera.position.z = 5
+
+//camera rezises
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+//orbitcontrols
+const controls = new OrbitControls(camera,canvas)
+controls.enableDamping = true
+
+//scene
+const scene = new THREE.Scene()
+
+scene
+    .add(camera)
+    .add(Box_object)
+    .add(axesHelper)
+
+//renderer
+const renderer = new THREE.WebGLRenderer({canvas})
+renderer.setSize(sizes.width,sizes.height)
+
+//timer control fps
+let time = Date.now()
+
+//loop
+const loop = () =>{
+    
+    //timer control fps
+    const currentTime = Date.now()
+    const deltaTime = currentTime - time
+    time = currentTime
+
+    //controls update
+    controls.update()
+
+    //renderer
+    renderer.render(scene,camera)
+
+    //recall loop
+    requestAnimationFrame(loop)
+}
+
+loop()
